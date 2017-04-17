f4ab0079d7f4070b02016d658cedf158a078ac21
martins-giberts <martins.giberts@gmail.com>
2017-04-17 16:02:33 +0300

    Styles adjustments, add aditional tests

--- app/elements/link/index.jsx
+++ app/elements/link/index.jsx
@@ -4,7 +4,7 @@ import styles from './Link.css'
 
 const Link = ({
   children,
-  href,
+  href = '#',
   target = '_blank',
 }) =>
   <a
--- app/elements/link/test.jsx
+++ app/elements/link/test.jsx
@@ -1,21 +1,50 @@
+/* globals describe, it, expect */
+
 import React from 'react'
 import {shallow} from 'enzyme'
 
 import Component from './index'
 
-describe('Sample test', function() {
+describe('elements/Link test', function() {
+  const component = shallow(
+    <Component href='#' target='_self'>
+      <span>Link</span>
+    </Component>
+  )
+
   it('should render without throwing an error', () => {
-    expect(shallow(<Component href='#' target='_blank'>Link</Component>)
-      .contains(<a className='link' href='#' target='_blank' >Link</a>))
-      .toBe(true)
+    expect(component
+      .contains(<a className='link' href='#' target='_self' ><span>Link</span></a>)
+    ).toBe(true)
   })
 
   it('should render children when passed in', () => {
-    const wrapper = shallow(
-      <Component href='#' target='_blank'>
-        <span>Link</span>
-      </Component>
-    )
-    expect(wrapper.contains(<span>Link</span>)).toBe(true)
+    expect(
+      component.contains(<span>Link</span>)
+    ).toBe(true)
+  })
+
+  it('should have prop href #', () => {
+    expect(
+      component.prop('href')
+    ).toBe('#')
+  })
+
+  it('should have prop target _self', () => {
+    expect(
+      component.prop('target')
+    ).toBe('_self')
+  })
+
+  it('should have default prop href #', () => {
+    expect(
+      shallow(<Component />).prop('href')
+    ).toBe('#')
+  })
+
+  it('should have default prop target _blank', () => {
+    expect(
+      shallow(<Component />).prop('target')
+    ).toBe('_blank')
   })
 })
