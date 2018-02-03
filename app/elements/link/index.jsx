919bceb327616d8305f37f439994ecc715363b44
Martins Giberts <martins.giberts@gmail.com>
2018-02-03 21:38:51 +0100

    Files Cleanup

--- app/elements/link/index.jsx
+++ app/elements/link/index.jsx
@@ -1,30 +1,8 @@
-// @flow
-// import React from 'react'
 import styled from 'styled-components'
-// import styles from './Link.css'
 
 const Link = styled.a`
   color: ${props => props.theme.primary};
   display: inline-block;
 `
-
-// type props = {
-//   children?: mixed,
-//   href: string,
-//   target?: '_blank' | '_self',
-// }
-
-// const Link = ({
-//   children,
-//   href = '#',
-//   target = '_blank',
-// }: props) =>
-//   <a
-//     className={styles.link}
-//     href={href}
-//     target={target}
-//   >
-//     {children}
-//   </a>
 
 export default Link
