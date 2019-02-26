610b20e76c8911e0fa463c67a9732a6174b8dbe0
Martins Giberts <martins.giberts@gmail.com>
2019-02-26 11:35:08 +0200

    Adjust min-height for app container

--- src/components/app/app-container.tsx
+++ src/components/app/app-container.tsx
@@ -2,14 +2,24 @@ import * as React from 'react';
 import styled, { StyledComponent } from 'styled-components';
 import { media } from '../theme';
 
+const getContainerSize = ({ docHeight }: { docHeight: number }): string => {
+  const maxHeight = 1440;
+
+  if (docHeight) {
+    return `min-height: ${maxHeight >= docHeight ? docHeight : maxHeight}px;`;
+  }
+
+  return '';
+};
+
 const StyledAppContainer: StyledComponent<'div', any, { docHeight?: number }, never> = styled.div`
   ${props => props.theme.fontStyles.primary}
+  ${getContainerSize}
   position: relative;
   display: flex;
   flex-direction: column;
 
   ${media.desktop`
-    ${(props: { docHeight: number }) => (props.docHeight ? `min-height: ${props.docHeight}px` : '')}
     width: 90rem;
     margin: 0 auto;
   `}
